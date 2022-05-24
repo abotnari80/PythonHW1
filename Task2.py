65b7a8f052e6b37a0eb4defcb4494b1f92b335f3
abotnari80 <abotnari80@gmail.com>
2022-05-24 17:05:31 +0300

    Задача 2

---
+++ Task2.py
@@ -0,0 +1,10 @@
+x = int(input("Введите значение x "))
+y = int(input("Введите значение y "))
+if x > 0 and y > 0:
+    print(1)
+elif x < 0 and y > 0:
+    print(2)
+elif x < 0 and y < 0:
+    print(3)
+elif x > 0 and y < 0:
+    print(4)
